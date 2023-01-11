eb196a9dd70636bac643559b0f5cd04ddd951434
Martín <magadiflo@gmail.com>
2023-01-11 10:02:51 -0500

    Implement JWT authentication filter

---
+++ src/main/java/com/magadiflo/app/config/JwtAuthFilter.java
@@ -0,0 +1,66 @@
+package com.magadiflo.app.config;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * JwtAuthFilter
+ * *************
+ * Para decirle a Spring que esta clase será una clase de filtro,
+ * es decir una clase de filtro personalizada donde la usaremos
+ * para la autenticación con JWT, necesitamos extender de una clase
+ * de filtro: OncePerRequestFilter.
+ * Eso significa que cada solicitud que se haga a nuestro backend
+ * primero será interceptado por ese filtro.
+ * <p>
+ * Debemos implementar el método doFilterInternal(...) del filtro para
+ * decirle a Spring lo que debemos hacer cuando interceptamos la solicitud.
+ */
+@Component
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        final String authHeader = request.getHeader("Authorization");
+        final String userEmail;
+        final String jwtToken;
+        final String PREFIX = "Bearer ";
+
+        if (authHeader == null || !authHeader.startsWith(PREFIX)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        jwtToken = authHeader.substring(PREFIX.length());
+        userEmail = "something"; // TODO to be implemented
+
+        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+            final boolean isTokenValid = false; // TODO to be implemented
+
+            if (isTokenValid) {
+                UsernamePasswordAuthenticationToken authToken =
+                        new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+}
